90d2c385818b2c9bb45d0f720ae18edfa27105e1
Kakeru Kikuta <kk1911shoter@icloud.com>
2023-09-13 14:31:01 +0900

    Prep2 comp

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("This program shows your grade. Enter your grade percentage.");
+        string enterdGrade = Console.ReadLine();
+        int grade = int.Parse (enterdGrade);
+
+        string output = "";
+
+        if (grade >= 90)
+        {
+            output = "A";
+        }
+        else if (grade >= 80)
+        {
+            output = "B";
+        }
+        else if (grade >= 70)
+        {
+            output = "C";
+        }
+        else if (grade >= 60)
+        {
+            output = "D";
+        }
+        else
+        {
+            output = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {output}");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
