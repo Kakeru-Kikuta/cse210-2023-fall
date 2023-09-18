c9269caa5773c4ba673d960f83d3d78e1d6537f8
Kakeru Kikuta <kk1911shoter@icloud.com>
2023-09-18 11:09:23 +0900

    Finish Job.cs, Finish job1

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Job
+{
+    public string _jobtitle;
+    public string _company;
+    public string _startYear;
+    public string _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobtitle} ({_company}) {_startYear}-{_endYear}")
+    }
+}
