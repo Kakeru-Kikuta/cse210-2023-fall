2b950486422206a16e26c1645d88212597923da6
Kakeru Kikuta <kk1911shoter@icloud.com>
2023-09-18 15:22:11 +0900

    build progressing

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Reflection;
 
-class Program
-{
-    static void Main(string[] args)
+namespace Demo
+{   
+    public class Program
     {
-        Console.WriteLine("Hello Develop02 World!");
+        static void Main(String[] args)
+        {
+
+            Prompt prompt = new Prompt();
+            prompt._prompt1 = "Who was the most interesting person I interested with todday?";
+            prompt._prompt2 = "What was the best part of my day?";
+            prompt._prompt3 = "How did I see the hand of the Lord in my life today?";
+            prompt._prompt4 = "What was the strongest emotion I felt today?";
+            prompt._prompt5 = "If I had one thing I could do over today, what would it be?";
+
+            List<Prompt> quesutions = new List<Prompt>();
+            quesutions.Add(prompt);
+            
+
+        }
+
+    
+
+
     }
+
 }
