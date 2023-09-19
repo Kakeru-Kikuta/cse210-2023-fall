dd061708ec254f6945b3d47de87ce1c8183fc13f
Kakeru Kikuta <kk1911shoter@icloud.com>
2023-09-19 16:31:20 +0900

    Main manu build

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,13 +1,37 @@
 using System;
-using System.Collections.Generic;
+using System.Data.Common;
+
 
 class Program
 {
-    static void Main(string[] args)
+    static void main(string[] arge)
     {
         Console.WriteLine("Welcome to your journal! What do you want to do? Enter a menu number. \r\n1, Whrite new Journal. \r\n2 Show all Journal. \r\n3 Save your Journal. \r\n4 Road your Journal.");
-        Console.ReadLine();
-        
-        
+        string _userMenuEnter = Console.ReadLine();
+
+        if(_userMenuEnter == "1")
+        {
+
+        }
+
+        else if(_userMenuEnter == "2")
+        {
+
+        }
+
+        else if(_userMenuEnter == "3")
+        {
+
+        }
+
+        else if (_userMenuEnter == "4")
+        {
+
+        }
+
+       
     }
+
+    
 }
+
