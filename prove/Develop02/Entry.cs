b5a90a4706d4982dc5945a7e88cd87b38ce5082f
Kakeru Kikuta <kk1911shoter@icloud.com>
2023-09-19 16:00:31 +0900

    menu test

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -9,14 +9,26 @@ namespace entryCode
 {
     public class Entry
     {
-      static void Main(string[] args)
-        {
-            Console.Write("Current Date and Time is: ");
-            DateTime now = DateTime.Now;
-            Console.WriteLine(now);
-            Console.ReadLine();
+        static void Main(string[] args)
+            {
+                Questions p1 = new Questions();
+                p1._prompt1 = "Who was the most interesting person I interested with todday?";
+                p1._prompt2 = "What was the best part of my day?";
+                p1._prompt3 = "How did I see the hand of the Lord in my life today?";
+                p1._prompt4 = "What was the strongest emotion I felt today?";
+                p1._prompt5 = "If I had one thing I could do over today, what would it be?";
 
-        }
+                List<Questions> question = new List<Questions>();
+                question.Add(p1);
+            }
+        static void Main(string[] args)
+            {
+                Console.Write("Current Date and Time is: ");
+                DateTime now = DateTime.Now;
+                Console.WriteLine(now);
+                Console.ReadLine();
+
+            }
 
         string fileName = "journal_history.txt";
         string journal_entry = outputFile.ReadLine();
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,33 +1,13 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-using System.IO;
-using System.Reflection;
 
-namespace Demo
-{   
-    public class Program
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(String[] args)
-        {
-
-            Prompt prompt = new Prompt();
-            prompt._prompt1 = "Who was the most interesting person I interested with todday?";
-            prompt._prompt2 = "What was the best part of my day?";
-            prompt._prompt3 = "How did I see the hand of the Lord in my life today?";
-            prompt._prompt4 = "What was the strongest emotion I felt today?";
-            prompt._prompt5 = "If I had one thing I could do over today, what would it be?";
-
-            List<Prompt> quesutions = new List<Prompt>();
-            quesutions.Add(prompt);
-            
-
-        }
-
-    
-
-
+        Console.WriteLine("Welcome to your journal! What do you want to do? Enter a menu number. \r\n1, Whrite new Journal. \r\n2 Show all Journal. \r\n3 Save your Journal. \r\n4 Road your Journal.");
+        Console.ReadLine();
+        
+        
     }
-
 }
